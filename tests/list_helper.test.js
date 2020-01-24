a16ac0bede1a2700b92f99df9e3299461b29e39f
Temez1 <heleniusteemu@gmail.com>
2020-01-24 12:58:26 +0200

    Added helper functions and tests for them

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -74,5 +74,52 @@ describe("total likes", () => {
 })
 
 describe("favorite blog", () => {
-  // TODO: add tests
+  test("of empty list is zero", () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(0)
+  })
+
+  test("when list has only one blog equals the given blog", () => {
+    const result = listHelper.favoriteBlog([blogs[0]])
+    expect(result).toEqual(blogs[0])
+  })
+
+  test("when list has many blogs equals the blog with most likes", () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(blogs[2])
+  })
+})
+
+describe("most blogs", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.mostBlogs([])
+    expect(result).toBe(0)
+  })
+
+  test("of list with one blog returns the author and the amount of blogs equals one", () => {
+    const result = listHelper.mostBlogs([blogs[0]])
+    expect(result).toEqual({ author: "Michael Chan", blogs: 1 })
+  })
+
+  test("of list with many blogs returns the author with most blogs", () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual({ author: "Robert C. Martin", blogs: 3 })
+  })
+})
+
+describe("most likes", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.mostLikes([])
+    expect(result).toBe(0)
+  })
+
+  test("of list with one blog returns the author and the amount of likes", () => {
+    const result = listHelper.mostLikes([blogs[0]])
+    expect(result).toEqual({ author: "Michael Chan", likes: 7 })
+  })
+
+  test("of list with many blogs returns the author with most likes", () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({ author: "Edsger W. Dijkstra", likes: 17 })
+  })
 })
